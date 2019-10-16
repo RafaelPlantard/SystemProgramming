2efcce23bed79d6ecac9841994da5be0500cc2e0
Cleís Aurora Silveira Pereira <cleis_aurora@yahoo.com.br>
2019-10-16 13:51:01 -0300

    Improve primeNumbers by brute-force ignoring even numbers

--- I/primeNumbers/PrimeNumbers.java
+++ I/primeNumbers/PrimeNumbers.java
@@ -1,12 +1,14 @@
 public class PrimeNumbers {
     public static void main(String[] args) {
-        int lower = 2;
-        int upper = 500;
+        int lower = 3;
+        int upper = 499;
 
-        for (int i = lower; i < upper; i++) {
+        System.out.println(2);
+
+        for (int i = lower; i <= upper; i += 2) {
             boolean isPrime = true;
 
-            for (int j = 2; j < i; j++) {
+            for (int j = 3; j < i; j += 2) {
                 if (i % j == 0) {
                     isPrime = false;
                     break;
